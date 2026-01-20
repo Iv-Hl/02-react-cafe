f113b66377292a7535c5bc1c60ed959473737e35
Iv-Hl <2020ivanko14@gmail.com>
2026-01-20 21:35:48 +0100

    developed till 5 step of description
    add functional buttons (good, neutral, bad)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import css from './App.module.css';
+import type { Votes, VoteType } from '../types/votes.ts'
+import VoteOptions from '../VoteOptions/VoteOptions.tsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [Vote, SetVote] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  function handleVote(type: VoteType) {
+    SetVote(prev => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  }
+  
+  function resetVotes() {
+    SetVote({
+      good: 0,
+      neutral: 0,
+      bad: 0
+    });
+  }
+
+  const totalVotes = Vote.good + Vote.neutral + Vote.bad;
+
+  const positiveRate = totalVotes ? Math.round((Vote.good / totalVotes) * 100) : 0;
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={positiveRate < 0} />
+    </div>
+  );
 }
 
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import { createRoot } from "react-dom/client";
-
+import App from "./components/App/App.tsx";
+import { StrictMode } from "react";
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <div>
-    <h1>Welcome to React</h1>
-    <p>
-      This is JSX — it looks like HTML, but it's not quite the same. It has its
-      own rules!
-    </p>
-  </div>,
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
