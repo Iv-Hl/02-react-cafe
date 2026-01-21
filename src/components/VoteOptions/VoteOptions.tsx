35be135a38c6c7b089f07310cf6ae8ebd3311556
Iv-Hl <2020ivanko14@gmail.com>
2026-01-21 17:57:09 +0100

    fixed path types

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,24 +1,36 @@
-import css from './VoteOptions.module.css';
-import type { VoteType } from '../types/votes.ts';
+import css from "./VoteOptions.module.css";
+import type { VoteType } from "../../types/votes.ts";
 
 interface VoteOptionsPromts {
-    onVote: (type: VoteType) => void,
-    onReset: () => void,
-    canReset: boolean
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
 }
 
-export default function VoteOptions({onVote, onReset, canReset}: VoteOptionsPromts) {
-    return (
-        <div className={css.container}>
-            <button className={css.button} onClick={() => onVote("good")}>Good</button>
+export default function VoteOptions({
+  onVote,
+  onReset,
+  canReset,
+}: VoteOptionsPromts) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote("good")}>
+        Good
+      </button>
 
-            <button className={css.button} onClick={() => onVote("neutral")}>Neutral</button>
+      <button className={css.button} onClick={() => onVote("neutral")}>
+        Neutral
+      </button>
 
-            <button className={css.button} onClick={() => onVote("bad")}>Bad</button>
+      <button className={css.button} onClick={() => onVote("bad")}>
+        Bad
+      </button>
 
-            {canReset && (
-                <button className={`${css.button} ${css.reset}`} onClick={onReset}>Reset</button>
-            )}
-        </div>
-    );
+      {canReset && (
+        <button className={`${css.button} ${css.reset}`} onClick={onReset}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
 }
